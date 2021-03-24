0661bdc17ad3554487e2e896aa7ebf95b5582e8c
ducciorocchini <ducciorocchini@gmail.com>
2021-03-24 12:57:30 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -4,8 +4,8 @@
 # install.packages("raster")
 library(raster)
 
-setwd("~/lab/") # Linux
-# setwd("C:/lab/") # Windows
+# setwd("~/lab/") # Linux
+setwd("C:/lab/") # Windows
 # setwd("/Users/name/Desktop/lab/") # Mac 
 
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
@@ -16,10 +16,84 @@ plot(p224r63_2011)
 # colour change
 cl <- colorRampPalette(c("black","grey","light grey")) (100) 
 plot(p224r63_2011, col=cl)
-                       
+
+#### DAY 2
+
 # colour change -> new
 cl <- colorRampPalette(c("blue","green","grey","red","magenta","yellow")) (100) 
 plot(p224r63_2011, col=cl)
 
 cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
 plot(p224r63_2011, col=cls)                       
+
+#### DAY 3
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+# dev.off will clean the current graph
+dev.off()
+
+plot(p224r63_2011$B1_sre)
+
+cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
+plot(p224r63_2011$B1_sre, col=cls)
+
+dev.off()
+
+
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# 1 row, 2 columns
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# 2 row, 1 columns
+par(mfrow=c(2,1)) # if you are using columns first: par(mfcol....)
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# plot the first four bands of Landsat
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+# a quadrat of bands...:
+par(mfrow=c(2,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+# a quadrat of bands...:
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (100)
+plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c("dark green","green","light green")) (100)
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c("dark red","red","pink")) (100)
+plot(p224r63_2011$B3_sre, col=clr)
+
+clnir <- colorRampPalette(c("red","orange","yellow")) (100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+
+
+
+
+
+
+
+
