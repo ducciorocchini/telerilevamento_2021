2a053ff32fec37701e8e5de12956686556b3c8b7
ducciorocchini <ducciorocchini@gmail.com>
2021-03-31 13:01:43 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,1 +1,11 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from Emanuela Cosma
+
+# install.packages("raster")
+library(raster)
+
+setwd("~/lab/greenland") # Linux
+# setwd("C:/lab/greenland") # Windows
+# setwd("/Users/name/Desktop/lab/greenland") # Mac 
 
