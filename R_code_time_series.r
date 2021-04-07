f04030d587ebc33058664dccb887c31cb8ae4527
ducciorocchini <ducciorocchini@gmail.com>
2021-04-07 13:05:08 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -4,8 +4,42 @@
 
 # install.packages("raster")
 library(raster)
+# library(rgdal)
 
 setwd("~/lab/greenland") # Linux
 # setwd("C:/lab/greenland") # Windows
 # setwd("/Users/name/Desktop/lab/greenland") # Mac 
+
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+# par
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+
+# list f files:
+list.files()
+
+rlist <- list.files(pattern="lst")
+rlist
+
+import <- lapply(rlist,raster)
+import
+
+TGr <- stack(import)
+plot(TGr)
+
+
+plotRGB(TGr, 1, 2, 3, stretch="Lin") 
+plotRGB(TGr, 2, 3, 4, stretch="Lin") 
+plotRGB(TGr, 4, 3, 2, stretch="Lin") 
+
+
+
+
 
