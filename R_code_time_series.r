418029f0df9ec105f372e41b0d7e4e9d55c5f970
ducciorocchini <ducciorocchini@gmail.com>
2021-04-09 11:03:26 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -3,7 +3,10 @@
 # Data and code from Emanuela Cosma
 
 # install.packages("raster")
+# install.packages("rasterVis")
 library(raster)
+library(rasterVis) 
+
 # library(rgdal)
 
 setwd("~/lab/greenland") # Linux
@@ -30,12 +33,51 @@ import <- lapply(rlist,raster)
 import
 
 TGr <- stack(import)
+TGr
 plot(TGr)
-
 
 plotRGB(TGr, 1, 2, 3, stretch="Lin") 
 plotRGB(TGr, 2, 3, 4, stretch="Lin") 
 plotRGB(TGr, 4, 3, 2, stretch="Lin") 
+
+levelplot(TGr)
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+levelplot(TGr,col.regions=cl, main="LST variation in time",
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+# Melt
+meltlist <- list.files(pattern="melt")
+melt_import <- lapply(meltlist,raster)
+melt <- stack(melt_import)
+melt
+
+levelplot(melt)
+
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+
+levelplot(melt_amount, col.regions=clb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
