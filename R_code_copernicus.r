edca512ca0f7fc247bcc35503b4f6551235beeef
ducciorocchini <ducciorocchini@gmail.com>
2021-04-14 13:07:40 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -5,5 +5,15 @@
 library(raster)
 library(ncdf4)
 
-setwd("~/lab/")
+setwd("~/lab/") # Linux
+# setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac 
 
+albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc")
+
+cl <- colorRampPalette(c('light blue','green','red','yellow'))(100) # 
+plot(albed0, col=cl)
+
+# resampling
+albedores <- aggregate(albedo, fact=100)
+plot(albedores, col=cl)
