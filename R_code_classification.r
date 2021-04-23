1bfadb0a230d03ff14d29145a5933655021ec225
ducciorocchini <ducciorocchini@gmail.com>
2021-04-23 10:01:09 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -3,7 +3,9 @@
 library(raster)
 library(RStoolbox)
 
-setwd("~/lab")
+setwd("~/lab/") # Linux
+# setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac 
 
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 so
@@ -25,4 +27,16 @@ plot(soc20$map,col=cl)
 # https://landsat.visibleearth.nasa.gov/view.php?id=80948
 
 # When John Wesley Powell led an expedition down the Colorado River and through the Grand Canyon in 1869, he was confronted with a daunting landscape. At its highest point, the serpentine gorge plunged 1,829 meters (6,000 feet) from rim to river bottom, making it one of the deepest canyons in the United States. In just 6 million years, water had carved through rock layers that collectively represented more than 2 billion years of geological history, nearly half of the time Earth has existed.
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
+
+gcc4 <- unsuperClass(gc, nClasses=4)
+plot(gcc4$map)
 
