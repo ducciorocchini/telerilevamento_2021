3249b63e86fdc5a486e72e4b4fcabf17a316c69d
ducciorocchini <ducciorocchini@gmail.com>
2021-04-23 09:24:10 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,8 +1,9 @@
 # R_code_classification.r
 
-setwd("~/lab")
 library(raster)
 library(RStoolbox)
+
+setwd("~/lab")
 
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 so
@@ -19,3 +20,9 @@ cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(soc20$map,col=cl)
 
 # Download Solar Orbiter data and proceed further!
+
+# Grand Canyon
+# https://landsat.visibleearth.nasa.gov/view.php?id=80948
+
+# When John Wesley Powell led an expedition down the Colorado River and through the Grand Canyon in 1869, he was confronted with a daunting landscape. At its highest point, the serpentine gorge plunged 1,829 meters (6,000 feet) from rim to river bottom, making it one of the deepest canyons in the United States. In just 6 million years, water had carved through rock layers that collectively represented more than 2 billion years of geological history, nearly half of the time Earth has existed.
+
