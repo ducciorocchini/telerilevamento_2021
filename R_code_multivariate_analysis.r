6a7a30816ffc4dc360205ab2dbbe236c0507bbe9
ducciorocchini <ducciorocchini@gmail.com>
2021-04-28 12:23:27 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -18,6 +18,26 @@ plot(p224r63_2011$B2_sre, p224r63_2011$B1_sre, col="red", pch=19, cex=2)
 
 pairs(p224r63_2011)
 
+# aggregate cells: resampling (ricampionamento)
+p224r63_2011res <- aggregate(p224r63_2011, fact=10)
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin")
+
+p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
+
+summary(p224r63_2011res_pca$model)
+
+# dev.off()
+plotRGB(p224r63_2011res_pca$map, r=1, g=2, b=3, stretch="lin")
+
+
+
+
+
+
+
 
 
 
