4679c2d82ed2065e78a052a9297840ce3f16aa1d
ducciorocchini <ducciorocchini@gmail.com>
2021-05-07 10:45:02 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -5,7 +5,7 @@ library(RStoolbox) # classification
 # install.packages("ggplot2")
 library(ggplot2)
 # install.packages("gridExtra")
-library(gridExtra)
+library(gridExtra) # for grid.arrange plotting
 
 setwd("~/lab/") # Linux
 # setwd("C:/lab/") # Windows
@@ -29,6 +29,58 @@ plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 p1 <- ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
 p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
 grid.arrange(p1, p2, nrow=2)
+
+# unsupervised classification
+d1c <- unsuperClass(defor1, nClasses=2)
+plot(d1c$map)
+# class 1: forest
+# class 2: agriculture
+
+# set.seed() would allow you to attain the same results ...
+
+d2c <- unsuperClass(defor2, nClasses=2)
+plot(d2c$map)
+# class 1: agriculture
+# class 2: forest
+
+d2c3 <- unsuperClass(defor2, nClasses=3)
+plot(d2c3$map)
+
+# frequencies
+freq(d1c$map)
+#   value  count
+# [1,]     1 306583
+# [2,]     2  34709
+
+s1 <- 306583 + 34709
+
+prop1 <- freq(d1c$map) / s1
+# prop forest: 0.8983012
+# prop agriculture: 0.1016988
+
+s2 <- 342726
+prop2 <- freq(d2c$map) / s2
+# prop forest: 0.5206958
+# prop agriculture: 0.4793042
+
+# build a dataframe
+cover <- c("Forest","Agriculture")
+percent_1992 <- c(89.83, 10.16)
+percent_2006 <- c(52.06, 47.93)
+
+percentages <- data.frame(cover, percent_1992, percent_2006)
+percentages
+
+# let's plot them!
+ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
+
+p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
+
+grid.arrange(p1, p2, nrow=1)
+
+
 
 
 
