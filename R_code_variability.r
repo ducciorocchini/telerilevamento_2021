6c519a3e29c2cb6e983213977d44be18a5e53cea
ducciorocchini <ducciorocchini@gmail.com>
2021-05-19 12:31:14 +0200

    Create R_code_variability.r

---
+++ R_code_variability.r
@@ -0,0 +1,45 @@
+# R_code_variability.r
+
+library(raster)
+library(RStoolbox)
+# install.packages("RStoolbox")
+
+setwd("~/lab/") # Linux
+# setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac
+
+sent <- brick("sentinel.png")
+
+# NIR 1, RED 2, GREEN 3
+# r=1, g=2, b=3
+plotRGB(sent, stretch="lin") 
+# plotRGB(sent, r=1, g=2, b=3, stretch="lin") 
+
+plotRGB(sent, r=2, g=1, b=3, stretch="lin") 
+
+nir <- sent$sentinel.1
+red <- sent$sentinel.2
+
+ndvi <- (nir-red) / (nir+red)
+plot(ndvi)
+cl <- colorRampPalette(c('black','white','red','magenta','green'))(100) # 
+plot(ndvi,col=cl)
+
+ndvisd3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
+
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(ndvisd3, col=clsd)
+
+# mean ndvi with focal
+ndvimean3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3), fun=mean)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(ndvimean3, col=clsd)
+
+
+ndvisd13 <- focal(ndvi, w=matrix(1/169, nrow=13, ncol=13), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(ndvisd13, col=clsd)
+
+
+
+
