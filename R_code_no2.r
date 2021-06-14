34fc2d472277fca49163691cedf9fdf1691cdbb5
ducciorocchini <ducciorocchini@gmail.com>
2021-06-14 11:41:02 +0200

    Update R_code_no2.r

--- R_code_no2.r
+++ R_code_no2.r
@@ -1,5 +1,6 @@
 # R_code_no2.r
 library(raster)
+library(RStoolbox) # here used for raster based multivariate analysis
 
 # 1. Set the working directory EN
 
@@ -36,6 +37,36 @@ par(mfrow=c(3,1))
 plot(EN01, col=cls, main="NO2 in January")
 plot(EN13, col=cls, main="NO2 in March")
 plot(ENdif, col=cls, main="Difference (January - March)")
+
+# 7. Import the whole set
+
+# list of files:
+rlist <- list.files(pattern="EN")
+rlist
+
+import <- lapply(rlist,raster)
+import
+
+EN <- stack(import)
+plot(EN, col=cls)
+
+# 8. Replicate the plot of images 1 and 13 using the stack
+par(mfrow=c(2,1))
+plot(EN$EN_0001, col=cls)
+plot(EN$EN_0013, col=cls)
+
+# 9. Compute a PCA over the 13 images
+
+ENpca <- rasterPCA(EN)
+
+summary(ENpca$model)
+
+dev.off()
+plotRGB(ENpca$map, r=1, g=2, b=3, stretch="lin")
+
+# 10. Compute the local variability (local standard deviation) of the first PC
+PC1sd <- focal(ENpca$map$PC1, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
+plot(PC1sd, col=cls)
 
 
 
